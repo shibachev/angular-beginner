571b29e10b11f58fac621f0b06322e6f3cfdf5a5
shibachev <il0vmg0mlabo@gmail.com>
2020-01-02 10:35:56 +0900

    feat(ng): enable app routing.

--- front/ng/src/app/app.module.ts
+++ front/ng/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductListComponent } from './product/product-list/product-list.component';
+import { ProductDetailComponent } from './product/product-detail/product-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductListComponent,
+    ProductDetailComponent
   ],
   imports: [
     BrowserModule,
