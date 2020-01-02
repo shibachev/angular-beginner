ad2b3811d3213e9c77ae5290a0b8c5b39dbaccd3
shibachev <il0vmg0mlabo@gmail.com>
2020-01-02 16:14:26 +0900

    feat(ng): add list table.

--- front/ng/src/app/shared/shared.module.ts
+++ front/ng/src/app/shared/shared.module.ts
@@ -1,5 +1,6 @@
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { MatDividerModule } from '@angular/material/divider';
+import { MatTableModule } from '@angular/material/table';
 import { CommonModule } from '@angular/common';
 
 @NgModule({
@@ -9,6 +10,7 @@ import { CommonModule } from '@angular/common';
   exports: [
     CommonModule,
     MatDividerModule,
+    MatTableModule,
   ],
   schemas: [
     CUSTOM_ELEMENTS_SCHEMA,
